888b9a69707c481a18c1ff9623a65bc8c93e30e6
Sri5275 <saketh1844@gmail.com>
2024-10-28 18:16:56 +0530

    Sixth Commit

--- routes/auth.js
+++ routes/auth.js
@@ -6,23 +6,23 @@ const Joi = require("joi");
 
 router.post("/", async(req,res) => {
     try{
+        //validate request body
         const{error} = validate(req.body);
         if(error)
             return res.status(400).send({message:error.details[0].message});
-        
+        // Check for existing user
         const user = await User.findOne({email:req.body.email});
         if(!user)
             return res.status(401).send({message:"Invalid Email or Password"});        
-       
-        const validPassword = await bcrypt.compare(
-            req.body.password, user.password
-        );
+        // Compare Passwords
+        const validPassword = await bcrypt.compare( req.body.password, user.password);
         if(!validPassword)
             return res.status(401).send({message:"Invalid Email or Password"});
-
+        // Generate token and respond
         const token = user.generateAuthToken();
         res.status(200).send({data:token,message:"Logged in successfully"});
     }catch(error){
+        console.error(error) // Log the error for debugging
         res.status(500).send({message:"Internal Server Error"});
     }
 })
